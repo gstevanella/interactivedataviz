3872d777f9bc5e14e8a75dce633bd219f73656f3
gstevanella <gstevanella@gradcenter.cuny.edu>
2022-02-22 14:39:08 -0500

    Task 1_3 completed

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,1 +1,29 @@
 console.log('hello world');
+
+
+var counterVal = 0;
+
+function incrementClick() {
+    updateDisplay(++counterVal);
+}
+
+function resetCounter() {
+    counterVal = 0;
+    updateDisplay(counterVal);
+}
+
+function updateDisplay(val) {
+    document.getElementById("counter-label").innerHTML = val;
+}
+
+const label = document.getElementById("new-label")
+const input = document.getElementById ("new-input")
+const button = document.getElementById ("new-submit")
+
+let UserName;
+
+function UserNameInput (){
+UserName = input.value
+label.innerText = "We know what your future holds " + UserName 
+input.value = ""
+}
